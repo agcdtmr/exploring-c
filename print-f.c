5c621e1740234d2d555ec3c4cbc96041ae98fa9c
adtmr <anjdeitmer@gmail.com>
2023-02-23 11:11:10 +0100

    Create print-f.c

---
+++ print-f.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int ageLearnedToRide = 5;
+
+    printf("I was %d years old when I learned to ride a bike.\n", ageLearnedToRide);
+    printf("I hope I still remember how to ride.");
+}
+
+// Notes
+/*
+symbol | type
+%d or %i |	int
+%f |	double or float
+%c |	char
+
+*/
+
+// Compiling
+/*
+1. gcc print-f.c -o print-f
+2. ./print-f
+
+or
+
+1. gcc print-f.c -o a.out
+2. ./print-f
+*/
