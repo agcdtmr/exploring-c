f511daaea4c1b3b9359f4282639bd4f80b4916a8
adtmr <anjdeitmer@gmail.com>
2023-02-23 11:11:13 +0100

    Create variable.c

---
+++ variable.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int number;
+    int test;
+    int example;
+    printf("Hello World!\n");
+}
+
+// Notes
+/*
+Names can be composed of upper and lower case letters, numbers, and underscores.
+The first character must be a letter (upper or lower case).
+No keywords are allowed as the full name (int is not allowed but int_count would work).
+*/
+
+// Compiling
+/*
+1. gcc variable.c -o variable
+2. ./variable
+
+or
+
+1. gcc variable.c -o a.out
+2. ./variable
+*/
