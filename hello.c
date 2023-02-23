f221d198d31255cef8595e86cb44424aff0cc027
adtmr <anjdeitmer@gmail.com>
2023-02-23 11:11:07 +0100

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
   // output a line
   printf("Hello World!\n");
 }
+
+// Compiling
+/*
+1. gcc hello.c -o hello
+2. ./hello
+
+or
+
+1. gcc hello.c -o a.out
+2. ./hello
+*/
