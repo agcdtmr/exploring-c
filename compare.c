8275732b1357f715561a9fb822440f7122ce9e84
adtmr <anjdeitmer@gmail.com>
2023-02-23 15:12:54 +0100

    Create compare.c

---
+++ compare.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int x = 5;
+    int y = 42;
+
+    // if (x < y)
+    // if (x > y)
+    if (x == y)
+    {
+        printf("Congratulations on setting up a comparison correctly!");
+    }
+}
+
+// Compiling
+/*
+1. gcc compare.c -o compare
+2. ./compare
+
+or
+
+1. gcc compare.c -o a.out
+2. ./compare
+*/
