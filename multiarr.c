801bd5d5cd9104b9e687d9596bada041a60b2270
adtmr <anjdeitmer@gmail.com>
2023-03-02 13:32:09 +0100

    Create multiarr.c

---
+++ multiarr.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // Create a two-dimensional nine-by-nine uninitialized array of integers called arr.
+    int arr[9][9];
+
+    // Create an initialized two-dimensional array called matrix that represents it.
+    int matrix[][3] = {{2, 9, 11}, {3, 1, 4}, {8, 3, 1}};
+}
